974e527ecf1d35dede67e388dce76ea1dee68f3c
Gonzalez Adrian <adrgon@gmail.com>
2024-02-02 20:13:30 -0300

    Configuracion custom de React

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import {createRoot} from 'react-dom/client'
+
+const App = ()=>{
+  return (
+    <h1>Hola Mundo</h1>
+  )
+}
+
+const container = document.getElementById('root');
+const root = createRoot(container);
+root.render(<App />);
